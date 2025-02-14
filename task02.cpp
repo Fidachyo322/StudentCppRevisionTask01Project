db73395a287272a55abdb2ed143a94c6cd14c6e3
Fidachyo322 <baconvevo@mail.ru>
2025-02-14 19:40:38 +0300

    Решил 3 таска

--- task02.cpp
+++ task02.cpp
@@ -27,5 +27,18 @@
 */
 
 int task02(int a, int b) {
-	return 0;
+	int result = 0;
+	if (a > b) {
+		result = 1;
+	}
+	else {
+		if (a < b) {
+			result = 2;
+		}
+		else {
+			result = 0;
+		}
+	}
+
+return result;
 }
